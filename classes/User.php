91e5d6d29dbb314305caab3192a1d03d3a751da9
msramalho <up201403027@fe.up.pt>
2017-10-28 03:54:08 +0100

    example of application of QueryBuilder

--- classes/User.php
+++ classes/User.php
@@ -1,16 +1,22 @@
 <?php
 
 require_once(dirname(__FILE__)."/QueryBuilder.php");
-class User{
-	public $id;
-	public $name;
-	public $age;
-	private $query;
-
-	public function __construct($id, $name, $age){
-		$this->id = $id;
+class User extends QueryBuilder{
+	//properties must be protected, but relax QueryBuilder allows them to be accessed as if they were public
+	//if they are public, update will have nothing to update when you do $user->name = "newName"
+	public $userId;//attributes that do not require update can be public
+	protected $name;
+	protected $age;
+	//protected $gender;//this property can be dynamically created and it will be updated, by doing $user->gender = "male"; $user->update();
+	//public static $primaryKeys = array("userId", "questionId");//define this static variable for custom primary keys or see QueryBuilder->setKey();
+	public function __construct($userId = null, $name = "", $age = 0){
+		$this->userId = $userId;
 		$this->name = $name;
 		$this->age = $age;
-		$this->query = new QueryBuilder($this);
+		parent::__construct();//call parent constructor
 	}
+
+    public function __toString(){//optional to string function
+        return sprintf("<br>USER: userId:%d - name:%s - age:%d", $this->userId, $this->name, $this->age);
+    }
 }
