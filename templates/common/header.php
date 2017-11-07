13595f8f6abd473348eded49f361073b6d648d4a
msramalho <up201403027@fe.up.pt>
2017-11-07 14:13:09 +0000

    updated defaults to contain array of css files

--- templates/common/header.php
+++ templates/common/header.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="<?= $PAGE["title"] ?>">
-	<head>
-		<title><?= $PAGE["title"] ?></title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="<?= $PAGE["CSSstyle"] ?>" rel="stylesheet">
-		<?php foreach ($PAGE["scripts"] as $scriptName): ?>
-			<script type="text/javascript" src="<?= $scriptName ?>" defer></script>
-		<?php endforeach; ?>
-	</head>
-	<body>
+<head>
+	<title><?= $PAGE["title"] ?></title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<?php foreach ($PAGE["styles"] as $style) : ?>
+		<link href="<?= $style ?>" rel="stylesheet">
+	<?php endforeach; ?>
+
+	<?php foreach ($PAGE["scripts"] as $scriptName) : ?>
+		<script type="text/javascript" src="<?= $scriptName ?>" defer></script>
+	<?php endforeach; ?>
+
+</head>
+<body>
