b7bb07387b9ab4ce6a1ec52d15cecf8b1fcf1999
msramalho <up201403027@fe.up.pt>
2017-11-07 12:23:41 +0000

    class User updated for our Class model

--- classes/User.php
+++ classes/User.php
@@ -5,18 +5,17 @@ class User extends QueryBuilder{
 	//properties must be protected, but relax QueryBuilder allows them to be accessed as if they were public
 	//if they are public, update will have nothing to update when you do $user->name = "newName"
 	public $userId;//attributes that do not require update can be public
-	protected $name;
-	protected $age;
+	protected $username;
+	protected $email;
+	protected $password;
 	//protected $gender;//this property can be dynamically created and it will be updated, by doing $user->gender = "male"; $user->update();
 	//public static $primaryKeys = array("userId", "questionId");//define this static variable for custom primary keys or see QueryBuilder->setKey();
-	public function __construct($userId = null, $name = "", $age = 0){
+	public function __construct($userId = null, $username = "", $email = "", $password = ""){
 		$this->userId = $userId;
-		$this->name = $name;
-		$this->age = $age;
-		parent::__construct();//call parent constructor
+		$this->username = $username;
+		$this->email = $email;
+		$this->password = $password;
+		parent::__construct();//call parent constructor, necessary for QueryBuilder
 	}
 
-    public function __toString(){//optional to string function
-        return sprintf("<br>USER: userId:%d - name:%s - age:%d", $this->userId, $this->name, $this->age);
-    }
 }
