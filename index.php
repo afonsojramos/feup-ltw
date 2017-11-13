381323f87966c6e4832c24900e1f70406ce7c550
msramalho <up201403027@fe.up.pt>
2017-11-13 22:40:03 +0000

    [base html] base index, login and register pages

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	//$PAGE["styles"][] = "public/css/index.css";//add a css file at the end
 	//$PAGE["scripts"] = array("public/js/index.js");//add the scripts needed
 	require_once(dirname(__FILE__)."./templates/common/header.php");
+	require_once(dirname(__FILE__)."./includes/common/choose_navbar.php");
 ?>
 
 <h1>Hello World</h1>
---
+++ register.php
@@ -0,0 +1,10 @@
+<?php
+	require_once(dirname(__FILE__)."./includes/common/defaults.php");
+	$PAGE["title"] .= " : Register";
+	require_once(dirname(__FILE__)."./templates/common/header.php");
+	require_once(dirname(__FILE__)."./includes/common/choose_navbar.php");
+?>
+
+<h1>Register page</h1>
+
+<?php require_once(dirname(__FILE__)."./templates/common/footer.php"); ?>
