73993463c1e21a9b459c1b0108f66ca867cce24b
msramalho <up201403027@fe.up.pt>
2017-11-14 23:33:56 +0000

    [navbar login] link to edit_profile.php

--- templates/common/navbar_login.php
+++ templates/common/navbar_login.php
@@ -2,5 +2,5 @@
 <div class="flex-container" id="menu">
 	<div id="index"><a href="index.php">Index</a></div>
 	<div id="logout"><a href="actions/logout.php">Logout</a></div>
-	<div id="deleteUsers"><a href="deleteUsers.php">Delete users</a></div>
+	<div id="editProfile"><a href="edit_profile.php">Edit Profile</a></div>
 </div>
