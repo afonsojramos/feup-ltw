08fdad9b3130802d3dae1f6fafa9f995a16e2608
Afonso Jorge Ramos <up201506239@fe.up.pt>
2017-11-14 23:42:10 +0000

    Enhancements to Login Control

--- login.php
+++ login.php
@@ -6,12 +6,34 @@
 	require_once(dirname(__FILE__)."/includes/common/choose_navbar.php");
 ?>
 
-<h1>Login page</h1>
+	<div class="grid">
 
-<form action="actions/login.php" method="post" id="loginMenu">
-	<input type="text" name="username" id="username" placeholder="username"><br/>
-	<input type="password" name="password" id="password" placeholder="password"><br/>
-	<input type="submit" value="Login">
-</form>
+      <form action="actions/login.php" method="post" id="loginMenu">
+
+        <header class="loginHeader">
+          <h3 class="loginTitle">Login</h3>
+        </header>
+
+        <div class="loginBody">
+
+          <div class="formField">
+            <input type="text" name="username" id="username" placeholder="Username" required>
+          </div>
+
+          <div class="formField">
+            <input type="password" name="password" id="password" placeholder="Password" required>
+          </div>
+
+        </div>
+
+        <footer class="loginFooter">
+          <input type="submit" value="Login">
+
+          <p><span class="icon"></span><a href="#">Forgot Password?</a></p>
+        </footer>
+
+      </form>
+
+    </div>
 
 <?php require_once(dirname(__FILE__)."/templates/common/footer.php"); ?>
