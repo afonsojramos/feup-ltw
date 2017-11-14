61a54527359c8dd72e228f7b7c45e204018b4f4f
Afonso Jorge Ramos <up201506239@fe.up.pt>
2017-11-14 23:56:27 +0000

    [CSS] CSS Improvements??

--- templates/common/navbar_logout.php
+++ templates/common/navbar_logout.php
@@ -2,5 +2,5 @@
 	<div id="index"><a href="index.php">Index</a></div>
 	<div id="login"><a href="login.php">Login</a></div>
 	<div id="register"><a href="register.php">Register</a></div>
-	<div id="deleteUsers"><a href="deleteUsers.php">Delete users</a></div>
+	<div id="deleteUsers"><a href="deleteUsers.php">Delete Users</a></div>
 </div>
