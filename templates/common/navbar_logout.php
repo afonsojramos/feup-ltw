16e84bff7ad066bf1546c8d415a37a9e6d882ee3
msramalho <up201403027@fe.up.pt>
2017-11-14 17:44:14 +0000

    [navbar] logout link

--- templates/common/navbar_logout.php
+++ templates/common/navbar_logout.php
@@ -1,5 +1,6 @@
 <ul>
 	<li><a href="index.php">Index</a></li>
 	<li><a href="login.php">Login</a></li>
+	<li><a href="actions/logout.php">Logout</a></li>
 	<li><a href="register.php">Register</a></li>
 </ul>
