acc1e8b63832f21cebbb9b845107d097a13a6752
msramalho <up201403027@fe.up.pt>
2017-11-14 12:25:57 +0000

    [login] basic form

--- login.php
+++ login.php
@@ -7,4 +7,10 @@
 
 <h1>Login page</h1>
 
+<form action="actions/login.php" method="post">
+	<input type="text" name="username" id="username" placeholder="username"><br/>
+	<input type="password" name="password" id="password" placeholder="password"><br/>
+	<input type="submit" value="Login">
+</form>
+
 <?php require_once(dirname(__FILE__)."./templates/common/footer.php"); ?>
