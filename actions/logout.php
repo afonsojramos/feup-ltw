7f0fb2991f575c4f0aa0d56fb361d3e3a34dfea3
msramalho <up201403027@fe.up.pt>
2017-11-14 17:44:24 +0000

    actions/logout.php

---
+++ actions/logout.php
@@ -0,0 +1,6 @@
+<?php
+	require_once(dirname(__FILE__)."/../includes/common/session.php");
+	require_once(dirname(__FILE__)."/../classes/User.php");
+
+	User::logout();
+	header("Location: ../index.php");
