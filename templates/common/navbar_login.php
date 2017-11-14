c7b09b1529f9f0ca52c75dd95538918c451cace9
msramalho <up201403027@fe.up.pt>
2017-11-14 23:08:42 +0000

    [navbar_login] filled with temporary links

---
+++ templates/common/navbar_login.php
@@ -0,0 +1,8 @@
+<h1>Logged In</h1>
+<div class="flex-container" id="menu">
+	<div id="index"><a href="index.php">Index</a></div>
+	<div id="login"><a href="login.php">Login</a></div>
+	<div id="logout"><a href="actions/logout.php">Logout</a></div>
+	<div id="register"><a href="register.php">Register</a></div>
+	<div id="deleteUsers"><a href="deleteUsers.php">Delete users</a></div>
+</div>
