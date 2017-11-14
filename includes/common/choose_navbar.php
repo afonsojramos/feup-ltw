e3935cb1f6d26008b5be4efa23e69e6c8b8aadc3
msramalho <up201403027@fe.up.pt>
2017-11-14 23:08:10 +0000

    [choose_navbar] is done

--- includes/common/choose_navbar.php
+++ includes/common/choose_navbar.php
@@ -1,4 +1,8 @@
 <?php
-	//TODO: check if there is a session and choose the right navbar
-	require_once(dirname(__FILE__)."/../../templates/common/navbar_logout.php");
-?>
+	require_once(dirname(__FILE__)."/session.php");
+
+	if (loggedIn()) {
+		require_once(dirname(__FILE__)."/../../templates/common/navbar_login.php");
+	} else {
+		require_once(dirname(__FILE__)."/../../templates/common/navbar_logout.php");
+	}
