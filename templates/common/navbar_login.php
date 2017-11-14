bf1b63a6d025fd8411283256cbee22fe3e0b334f
msramalho <up201403027@fe.up.pt>
2017-11-14 23:20:29 +0000

    [navbar] removed register from navbar_login

--- templates/common/navbar_login.php
+++ templates/common/navbar_login.php
@@ -2,6 +2,5 @@
 <div class="flex-container" id="menu">
 	<div id="index"><a href="index.php">Index</a></div>
 	<div id="logout"><a href="actions/logout.php">Logout</a></div>
-	<div id="register"><a href="register.php">Register</a></div>
 	<div id="deleteUsers"><a href="deleteUsers.php">Delete users</a></div>
 </div>
