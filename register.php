a307ff65cd46ac2563b3cdbb173c0b6986d67c39
msramalho <up201403027@fe.up.pt>
2017-11-14 00:44:05 +0000

    basic register page

--- register.php
+++ register.php
@@ -7,4 +7,11 @@
 
 <h1>Register page</h1>
 
+<form action="actions/register.php" method="post">
+	<input type="text" name="username" id="username" placeholder="username"><br/>
+	<input type="email" name="email" id="email" placeholder="email"><br/>
+	<input type="password" name="password" id="password" placeholder="password"><br/>
+	<input type="submit" value="registar">
+</form>
+
 <?php require_once(dirname(__FILE__)."./templates/common/footer.php"); ?>
