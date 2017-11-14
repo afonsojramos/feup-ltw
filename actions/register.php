406a2e5af166b0f978a718e1f6fc9b24ca71fd51
msramalho <up201403027@fe.up.pt>
2017-11-14 00:44:14 +0000

    basic actions register

---
+++ actions/register.php
@@ -0,0 +1,16 @@
+<?php
+require_once(dirname(__FILE__)."./../classes/User.php");
+
+$result = array("success"=>false);
+
+$user = new User();
+$user->loadFromArray($_POST);//load user properties from post
+
+if($user->validate()){
+	if($user->insert()){
+		$result["success"] = true;
+	}
+}else{
+	$result["errors"] = $user->errors;
+}
+echo json_encode($result);
