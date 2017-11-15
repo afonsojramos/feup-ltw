9710a34da63c0fa6f1819bb65652f0e963ff6984
msramalho <up201403027@fe.up.pt>
2017-11-15 17:11:08 +0000

    [defaults script] updated the way the files are referenced
    now we only need to say the css/js filenames and not their path

--- includes/common/defaults.php
+++ includes/common/defaults.php
@@ -1,7 +1,9 @@
 <?php
 	//Set the default values for the properties used
+	$PAGE["cssFolder"] = "public/css/";
+	$PAGE["jsFolder"] = "public/js/";
 	$PAGE["lang"] = "en";
 	$PAGE["title"] = "4Me2Do";
-	$PAGE["styles"] = array("public/css/style.css");
+	$PAGE["styles"] = array("style.css", "navbar.css", "material-icons.css");
 	$PAGE["scripts"] = array();
 ?>
--- templates/common/header.php
+++ templates/common/header.php
@@ -6,11 +6,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<?php foreach ($PAGE["styles"] as $style) : ?>
-		<link href="<?= $style ?>" rel="stylesheet">
+		<link href="<?= $PAGE["cssFolder"] . $style ?>" rel="stylesheet">
 	<?php endforeach; ?>
 
 	<?php foreach ($PAGE["scripts"] as $scriptName) : ?>
-		<script type="text/javascript" src="<?= $scriptName ?>" defer></script>
+		<script type="text/javascript" src="<?= $PAGE["jsFolder"] . $scriptName ?>" defer></script>
 	<?php endforeach; ?>
 
 </head>
