cc525def69cbfacb46ee287d34f1c3476ec47332
msramalho <up201403027@fe.up.pt>
2017-11-16 01:53:05 +0000

    [modal] css + js

---
+++ public/js/modal.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let modals = document.getElementsByClassName("modal"); //get all modals
+
+for (const modal of modals) {
+	let openerId;
+	if (openerId = modal.getAttribute("opener")) {
+		let btnThatOpens = document.getElementById(modal.getAttribute("opener"));
+		let span = modal.getElementsByClassName("closeModal")[0];
+		if (btnThatOpens) {
+			btnThatOpens.onclick = function () {
+				modal.style.display = "block";
+			}
+			window.onclick = function (event) {
+				if (event.target == modal) {
+					modal.style.display = "none";
+				}
+			}
+			span.onclick = function () {
+				modal.style.display = "none";
+			}
+		}
+	}
+}
