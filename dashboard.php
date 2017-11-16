3a0e81c2e8fe9eafeec5f2a80f8cedbd102cc9ba
msramalho <up201403027@fe.up.pt>
2017-11-16 14:57:49 +0000

    [dashboard] with some lists

--- dashboard.php
+++ dashboard.php
@@ -1,36 +1,58 @@
 <?php
-	require_once(dirname(__FILE__)."/includes/common/only_allow_login.php");
-	require_once(dirname(__FILE__)."/includes/common/defaults.php");
-	$PAGE["title"] .= " : Dashboard for " . $_SESSION["username"];
-	$PAGE["styles"][] = "modal.css";
-	$PAGE["scripts"][] = "modal.js";
+    require_once(dirname(__FILE__)."/includes/common/only_allow_login.php");
+    require_once(dirname(__FILE__)."/includes/common/defaults.php");
+    $PAGE["title"] .= " : Dashboard for " . $_SESSION["username"];
+    $PAGE["styles"][] = "modal.css";
+    $PAGE["styles"][] = "todo_list.css";
+    $PAGE["scripts"][] = "modal.js";
 
-	require_once(dirname(__FILE__)."/templates/common/header.php");
-	require_once(dirname(__FILE__)."/includes/common/choose_navbar.php");
+    require_once(dirname(__FILE__)."/templates/common/header.php");
+    require_once(dirname(__FILE__)."/includes/common/choose_navbar.php");
 
-	require_once(dirname(__FILE__)."/templates/dashboard/sidebar.php");
+    require_once(dirname(__FILE__)."/templates/dashboard/sidebar.php");
 ?>
 
 
 <div class="container">
-	<h1>Welcome to the dashboard</h1>
-	<p>Let the todos begin.</p>
-	<div class="modal" opener="openModal">
-  		<div class="modalContent">
-			<div class="modalHeader">
-				<span class="closeModal"><i class="material-icons">close</i></span>
-				<h2>Modal Header</h2>
-			</div>
-			<div class="modalBody">
-				<p>Some text in the Modal Body</p>
-				<p>Some other text...</p>
-			</div>
-				<div class="modalFooter">
-				<h3>Modal Footer</h3>
-			</div>
-		</div>
-	</div>
-	<a id="openModal">Open Modal</a>
+    <h1>Welcome to the dashboard</h1>
+    <p>Let the todos begin.</p>
+
+    <div class="todos">
+		<div class="todo"> asdasd </div>
+    	<div class="todo"><img src="public/images/background.jpeg" width="200px" height="auto"></div>
+    	<div class="todo"><img src="public/images/logo_ltw.png" width="auto" height="200px"></div>
+    	<div class="todo"> asdasd </div>
+    	<div class="todo"> asafasdf assag sfd hgd hgd  hfh fhfdasd </div>
+    	<div class="todo"><img src="public/images/logo_ltw.png" width="auto" height="200px"></div>
+    	<div class="todo"><img src="public/images/background.jpeg" width="200px" height="auto"></div>
+    	<div class="todo"><img src="public/images/logo_ltw.png" width="auto" height="200px"></div>
+    	<div class="todo"> asafasdf assag sfd hgd hgd  hfh fhfdasd </div>
+    	<div class="todo"><img src="public/images/logo_ltw.png" width="auto" height="200px"></div>
+    	<div class="todo"><img src="public/images/background.jpeg" width="200px" height="auto"></div>
+    	<div class="todo"> asdasd </div>
+    	<div class="todo"> asdasd </div>
+    	<div class="todo"><img src="public/images/background.jpeg" width="200px" height="auto"></div>
+    	<div class="todo"> asdasd </div>
+    	<div class="todo"> asafasdf assag sfd hgd hgd  hfh fhfdasd </div>
+    </div>
+
+
+    <div class="modal" opener="openModal">
+        <div class="modalContent">
+            <div class="modalHeader">
+                <span class="closeModal"><i class="material-icons">close</i></span>
+                <h2>Modal Header</h2>
+            </div>
+            <div class="modalBody">
+                <p>Some text in the Modal Body</p>
+                <p>Some other text...</p>
+            </div>
+                <div class="modalFooter">
+                <h3>Modal Footer</h3>
+            </div>
+        </div>
+    </div>
+    <a id="openModal">Open Modal</a>
 </div><!--/.container-->
 
 
