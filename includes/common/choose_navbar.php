be3bc0269c08257b2c599877c1daab4f80dd737e
Daniel Silva <danny@dannyps.net>
2017-11-21 11:33:27 +0000

    Renaming navbar_lougout to navbar_guest

--- includes/common/choose_navbar.php
+++ includes/common/choose_navbar.php
@@ -4,5 +4,5 @@
 	if (loggedIn()) {
 		require_once(dirname(__FILE__)."/../../templates/common/navbar_login.php");
 	} else {
-		require_once(dirname(__FILE__)."/../../templates/common/navbar_logout.php");
+		require_once(dirname(__FILE__)."/../../templates/common/navbar_guest.php");
 	}
