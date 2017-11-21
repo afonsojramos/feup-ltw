05652fb863e07b9aa2bcb3a8e36c02d4a3ad77b7
msramalho <up201403027@fe.up.pt>
2017-11-21 12:51:13 +0000

    [modal aff list] template

---
+++ templates/dashboard/modal_add_list.php
@@ -0,0 +1,20 @@
+
+<div class="modal" opener="openAddListModal">
+	<div class="modalContent cardForm grid">
+		<div class="formHeader">
+			<h3 class="formTitle">New Todo List</h3>
+		</div>
+		<div class="formBody">
+			<div class="formField">
+				<input type="text" name="title" id="title" placeholder="List title" required>
+			</div>
+			<div class="formField">
+				<input type="text" name="tags" id="tags" placeholder="Tags (Comma separate)">
+			</div>
+		</div>
+		<footer class="formFooter">
+			<input class="closeModal" type="submit" value="Add">
+		</footer>
+	</div>
+</div>
+<button id="openAddListModal" class="fab fixed fabAccent">+</button>
