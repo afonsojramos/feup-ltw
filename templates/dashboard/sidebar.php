4f84bd1365599681a4b234384355e22f09d8efeb
Afonso Jorge Ramos <up201506239@fe.up.pt>
2017-11-21 12:54:33 +0000

    [HTML] Sidebar

---
+++ templates/dashboard/sidebar.php
@@ -0,0 +1,10 @@
+<div id="mySidenav" class="sidenav">
+<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+<a href="#">Project 1</a>
+<a href="#">Project 2</a>
+<a href="#">Project 3</a>
+<a href="#">Project 4</a>
+</div>
+
+<!-- Use any element to open the sidenav -->
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776Sidebar</span>
