27447240e2d751035806e12cc492d1fd678a2612
Afonso Jorge Ramos <up201506239@fe.up.pt>
2017-11-22 02:01:33 +0000

    [HTML] More control

--- templates/dashboard/sidebar.php
+++ templates/dashboard/sidebar.php
@@ -1,10 +1,38 @@
 <div id="mySidenav" class="sidenav">
 	<a class="closebtn" onclick="toggleSideBar()">&times;</a>
-
-	<a href="">My Lists</a>
-	<hr/>
-	<a href="">Project 1</a>
-	<a href="">Project 2</a>
-	<a href="">Project 3</a>
-	<a href="">Project 4</a>
+	<ul class="sidebar">
+		<li class="dropdown">
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown">My Lists<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+				<li>
+					<a href="#">What</a>
+				</li>
+				<li>
+					<a href="#">I wanted was to</a>
+				</li>
+				<li>
+					<a href="#">make this a dropdown</a>
+				</li>
+				<li>
+					<a href="#">However it is not done</a>
+				</li>
+				<li>
+					<a href="#">yet because...</a>
+				</li>
+			</ul>
+		</li>
+		<hr/>
+		<li>
+			<a href="">Project 1</a>
+		</li>
+		<li>
+			<a href="#">Project 2</a>
+		</li>
+		<li>
+			<a href="#">Project 3</a>
+		</li>
+		<li>
+			<a href="#">Project 4</a>
+		</li>
+	</ul>
 </div>
