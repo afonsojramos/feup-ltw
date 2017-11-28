0a21633ff0a5a596478e823c0f6b7dba93a6f52a
msramalho <up201403027@fe.up.pt>
2017-11-28 21:04:25 +0000

    [sidebar] some changes
    removed dropodown because it would get too big

--- templates/dashboard/sidebar.php
+++ templates/dashboard/sidebar.php
@@ -2,28 +2,12 @@
 	<a class="closebtn" onclick="toggleSideBar()">&times;</a>
 	<ul class="sidebar">
 		<li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown">My Lists<span class="caret"></span></a>
-			<ul class="dropdown-menu">
-				<li>
-					<a href="#">What</a>
-				</li>
-				<li>
-					<a href="#">I wanted was to</a>
-				</li>
-				<li>
-					<a href="#">make this a dropdown</a>
-				</li>
-				<li>
-					<a href="#">However it is not done</a>
-				</li>
-				<li>
-					<a href="#">yet because...</a>
-				</li>
-			</ul>
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="material-icons">lock_outline</i> My Lists</a>
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="material-icons">lock_open</i> All Lists</a>
 		</li>
 		<hr/>
 		<li>
-			<a href="">Project 1</a>
+			<a href="#">Project 1</a>
 		</li>
 		<li>
 			<a href="#">Project 2</a>
