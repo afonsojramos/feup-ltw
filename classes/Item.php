b848625f80c1569df6918072233b3188b9bfb35a
Afonso Jorge Ramos <up201506239@fe.up.pt>
2017-11-28 12:26:15 +0000

    [php] Item

---
+++ classes/Item.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once(dirname(__FILE__)."/QueryBuilder.php");
+class Item extends QueryBuilder{
+	public $itemId;//attributes that do not require update can be public
+	protected $completed;
+	protected $content;
+	protected $dueDate;
+	protected $todoListId;
+
+	public function __construct($itemId = null, $completed = false, $content = "", $dueDate = null, $todoListId = -1){
+		$this->itemId = $itemId;
+		$this->completed = $completed;
+		$this->content = $content;
+		if ($dueDate == null)
+			$this->dueDate = date('Y-m-d H:i:s');
+		else
+			$this->dueDate = $dueDate;
+		$this->todoListId = $todoListId;
+		parent::__construct();//call parent constructor, necessary for QueryBuilder
+	}
+
+	public function loadContent(){
+		//TODO load this item's content
+	}
+}
