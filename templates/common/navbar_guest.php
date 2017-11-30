ac2a6e71ddfa533c333908dd136ae914d4f61dab
Afonso Jorge Ramos <up201506239@fe.up.pt>
2017-11-30 11:10:56 +0000

    Sidebar minor improvements

--- templates/common/navbar_guest.php
+++ templates/common/navbar_guest.php
@@ -2,8 +2,8 @@
 	<div class="container">
 		<h1 class="logo"><a href="index.php"><i class="material-icons md-36">list</i> 4Me2Do</a></h1>
 		<ul class="nav navRight">
-			<li><a href="login.php">Login</a></li>
-			<li><a href="register.php">Register</a></li>
+			<li><i class="material-icons">person_outline</i><a href="login.php"> Login</a></li>
+			<li><i class="material-icons">person_add</i><a href="register.php"> Register</a></li>
 			<li><a href="deleteUsers.php">Delete Users</a></li>
 		</ul>
 	</div>
--- templates/dashboard/sidebar.php
+++ templates/dashboard/sidebar.php
@@ -2,8 +2,9 @@
 	<a class="closebtn" onclick="toggleSideBar()">&times;</a>
 	<ul class="sidebar">
 		<li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="material-icons">lock_outline</i> My Lists</a>
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="material-icons"  >lock_outline</i> My Lists</a>
 			<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="material-icons">lock_open</i> All Lists</a>
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="material-icons archived">archive</i> Archived</a>
 		</li>
 		<hr/>
 		<li>
