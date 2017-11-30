4c2e90b0936ce95f1286120f2c4f844e0ef0ee50
msramalho <up201403027@fe.up.pt>
2017-11-30 11:56:51 +0000

    [dashboard] todo item done

--- templates/dashboard/todo.php
+++ templates/dashboard/todo.php
@@ -1,9 +1,10 @@
 <div class="todo show-on-hover-parent color-<?= $todo->colour ?>" id = "todo_<?= $todo->todoListId; ?>"  data-todoListId="<?= $todo->todoListId ?>" >
 	<h3 class="noMargin"><?= htmlentities($todo->title) ?></h3>
 	<?php foreach ($todo->items as $item) : ?>
-		<div>
+		<div class="todoListItem">
 			<input class="checkbox todoItem" id = "todoItem_<?= $item->itemId ?>" data-itemId="<?= $item->itemId ?>" type="checkbox"  <?= $item->completed ? "checked" : "" ?>><label class="todoItemLabel" data-itemId="<?= $item->itemId ?>" ><?= $item->content ?></label>
 			<input type="text" class="addItemTextBox hidden" id="editItem_<?= $item->itemId ?>">
+			<i class="material-icons close removeListItem">close</i>
 		</div>
 	<?php endforeach ?>
 	<div class="addItemContainer">
