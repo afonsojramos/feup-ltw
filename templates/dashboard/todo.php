58c7315b4fb8babb22cfd13e3aecc915a052e42f
msramalho <up201403027@fe.up.pt>
2017-11-30 12:05:34 +0000

    [dashboard] minor change

--- templates/dashboard/todo.php
+++ templates/dashboard/todo.php
@@ -4,7 +4,7 @@
 		<div class="todoListItem">
 			<input class="checkbox todoItem" id = "todoItem_<?= $item->itemId ?>" data-itemId="<?= $item->itemId ?>" type="checkbox"  <?= $item->completed ? "checked" : "" ?>><label class="todoItemLabel" data-itemId="<?= $item->itemId ?>" ><?= $item->content ?></label>
 			<input type="text" class="addItemTextBox hidden" id="editItem_<?= $item->itemId ?>">
-			<i class="material-icons close removeListItem">close</i>
+			<i class="material-icons close removeListItem show-on-hover">close</i>
 		</div>
 	<?php endforeach ?>
 	<div class="addItemContainer">
