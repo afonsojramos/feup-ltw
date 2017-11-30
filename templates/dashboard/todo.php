cbe318f755610bbd4f478666bd7902cc1902e654
msramalho <up201403027@fe.up.pt>
2017-11-30 11:38:52 +0000

    [dashboard] html for items

--- templates/dashboard/todo.php
+++ templates/dashboard/todo.php
@@ -2,13 +2,20 @@
 	<h3 class="noMargin"><?= htmlentities($todo->title) ?></h3>
 	<?php foreach ($todo->items as $item) : ?>
 		<div>
-			<input class="checkbox todoItem" id = "todoItem_<?= $item->itemId ?>" data-itemId="<?= $item->itemId ?>" type="checkbox"  <?= $item->completed ? "checked" : "" ?>><label class="todoItemLabel" for="todoItem_<?= $item->itemId ?>"><?= $item->content ?></label>
+			<input class="checkbox todoItem" id = "todoItem_<?= $item->itemId ?>" data-itemId="<?= $item->itemId ?>" type="checkbox"  <?= $item->completed ? "checked" : "" ?>><label class="todoItemLabel" data-itemId="<?= $item->itemId ?>" ><?= $item->content ?></label>
+			<input type="text" class="addItemTextBox hidden" id="editItem_<?= $item->itemId ?>">
 		</div>
 	<?php endforeach ?>
+	<div class="addItemContainer">
+		<span>
+			<i class="material-icons">add</i>
+			<span class="addItemText"> Add a new item</span>
+		</span>
+	</div>
 	<div class="errors"></div>
 	<hr/>
 	<span class="listFooter show-on-hover">
-		<span class="archive"><a href="#"><i class="material-icons">archive</i></a></span>
+		<span class="archive"><a href="#"><i class="material-icons"><?= $todo->archived ? "unarchive" : "archive" ?></i></a></span>
 		<span class="delete"><a href="#"><i class="material-icons">delete</i></a></span>
 		<span class="share"><a href="#"><i class="material-icons">share</i></a></span>
 	</span>
