5c3fd46445a0832de8f8f4d95e3047538850d077
msramalho <up201403027@fe.up.pt>
2017-11-30 12:05:20 +0000

    [actions] fixed missing parentheses

--- actions/delete_item.php
+++ actions/delete_item.php
@@ -1,31 +1,31 @@
 <?php
-require_once(dirname(__FILE__)."/../includes/common/only_allow_login.php");
-require_once(dirname(__FILE__)."/../classes/Item.php");
-require_once(dirname(__FILE__)."/../classes/TodoList.php");
+require_once(dirname(__FILE__) . "/../includes/common/only_allow_login.php");
+require_once(dirname(__FILE__) . "/../classes/Item.php");
+require_once(dirname(__FILE__) . "/../classes/TodoList.php");
 
-$result = array("success"=>false);
+$result = array("success" => false);
 
 $item = new Item();
 
-if($item->load($_POST["itemId"])){
+if ($item->load($_POST["itemId"])) {
 	$item->todoListId;
 
 	$todoList = new TodoList();
 
-	if($todoList->load($item->todoListId)){
-		if ($todoList->verifyOwnership($_SESSION["userId"]){
-			if ($item->delete() !== false){
+	if ($todoList->load($item->todoListId)) {
+		if ($todoList->verifyOwnership($_SESSION["userId"])) {
+			if ($item->delete() !== false) {
 				$result["success"] = true;
-			} else{
-				$result["errors"]= array("Could not delete item");
+			} else {
+				$result["errors"] = array("Could not delete item");
 			}
-		} else{
-			$result["errors"]= array("User is not the owner of the todoList");
+		} else {
+			$result["errors"] = array("User is not the owner of the todoList");
 		}
-	}else {
+	} else {
 		$result["errors"] = array("Could not load todoListId");
 	}
-}else {
+} else {
 	$result["errors"] = array("Could not load itemId");
 }
 
--- actions/edit_item.php
+++ actions/edit_item.php
@@ -1,35 +1,35 @@
 <?php
-require_once(dirname(__FILE__)."/../includes/common/only_allow_login.php");
-require_once(dirname(__FILE__)."/../classes/Item.php");
-require_once(dirname(__FILE__)."/../classes/TodoList.php");
+require_once(dirname(__FILE__) . "/../includes/common/only_allow_login.php");
+require_once(dirname(__FILE__) . "/../classes/Item.php");
+require_once(dirname(__FILE__) . "/../classes/TodoList.php");
 
-$result = array("success"=>false);
+$result = array("success" => false);
 
 $item = new Item();
 
-if($item->load($_POST["itemId"])){
+if ($item->load($_POST["itemId"])) {
 	$item->todoListId;
 
 	$todoList = new todoList();
-	if($todoList->load($item->todoListId)){
-		if ($todoList->verifyOwnership($_SESSION["userId"]){
-			if($_POST["action"] == "completed"){
-				$item->completed = $_POST["completed"]=="false"?0:1;
-			} else if($_POST["action"] == "content"){
+	if ($todoList->load($item->todoListId)) {
+		if ($todoList->verifyOwnership($_SESSION["userId"])) {
+			if ($_POST["action"] == "completed") {
+				$item->completed = $_POST["completed"] == "false" ? 0 : 1;
+			} else if ($_POST["action"] == "content") {
 				$item->content = $_POST["content"];
 			}
-			if ($item->update() !== false{
+			if ($item->update() !== false) {
 				$result["success"] = true;
-			} else{
-				$result["errors"]= array("Could not update item");
+			} else {
+				$result["errors"] = array("Could not update item");
 			}
-		} else{
-			$result["errors"]= array("No match for userId");
+		} else {
+			$result["errors"] = array("No match for userId");
 		}
-	}else {
+	} else {
 		$result["errors"] = array("Could not load todoListId");
 	}
-}else {
+} else {
 	$result["errors"] = array("Could not load itemId");
 }
 
