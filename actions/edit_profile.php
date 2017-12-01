6fed1f954cb3797c1487db738dd52abd4538144c
msramalho <up201403027@fe.up.pt>
2017-12-01 22:16:42 +0000

    [edit_profile] replaced session verification by require

--- actions/edit_profile.php
+++ actions/edit_profile.php
@@ -4,72 +4,64 @@
  * This action file treats 2 kinds of requests:
  * 	- chnage of user name and/or email.
  *  - change of password.
- * 
+ *
  * These are two separate actions, and cannot be done concurrently.
- * 
+ *
  */
 
-require_once(dirname(__FILE__)."/../classes/User.php");
-require_once(dirname(__FILE__)."/../includes/common/session.php");
-$result = array("success"=>false);
+require_once(dirname(__FILE__) . "/../includes/common/only_allow_login.php");
+require_once(dirname(__FILE__) . "/../classes/User.php");
 
-
-if(!isset($_SESSION['userId']) || $_SESSION['userId']==''){
-	$result["errors"] = array("session expired");
-	echo json_encode($result);
-	die();
-}
+$result = array("success" => false);
 
 verifyCSRF($_POST['csrf']);
 
-if(isset($_POST['pwd1']) && isset($_POST['pwd2']) && $_POST['pwd1']!="" && $_POST['pwd2']!=""){
+if (isset($_POST['pwd1']) && isset($_POST['pwd2']) && $_POST['pwd1'] != "" && $_POST['pwd2'] != "") {
 	/**
 	 * This is a password change. Let's treat it acordingly.
 	 */
 
 	$user = new User();
-	if(!$user->load($_SESSION['userId'])){ //load user
+	if (!$user->load($_SESSION['userId'])) { //load user
 		$result["errors"] = array("username no longer exists");
 	}
-	
-	if($_POST['pwd1']!==$_POST['pwd2']){
+
+	if ($_POST['pwd1'] !== $_POST['pwd2']) {
 		$result["errors"] = array("passwords don't match");
-	}else{
-		$user->password=$_POST['pwd1'];
+	} else {
+		$user->password = $_POST['pwd1'];
 	}
-	
-	if($user->validate()){
-		if($user->clear()->update() == 1){
+
+	if ($user->validate()) {
+		if ($user->clear()->update() == 1) {
 			$result["success"] = true;
 		}
-	}else{ 
+	} else {
 		$result["errors"] = $user->errors;
 	}
 
-
-}else{
+} else {
 	/**
 	 * This is a username/email change.
 	 */
 
 	$user = new User();
-	if(!$user->load($_SESSION['userId'])){ //load user
-		die('User no longer exists!');
+	if (!$user->load($_SESSION['userId'])) { //load user
+		$result["errors"] = array("username no longer exists");
 	}
-	
-	$user->username=$_POST['username'];
-	$user->email=$_POST['email'];
-	
-	
-	if($user->validate()){
-		if($user->duplicateUsernameOnEditProfile()){
+
+	$user->username = $_POST['username'];
+	$user->email = $_POST['email'];
+
+	if ($user->validate()) {
+		if ($user->duplicateUsernameOnEditProfile()) {
 			$result["errors"] = array("username already exists");
-		}elseif($user->duplicateEmailOnEditProfile()){
+		} elseif ($user->duplicateEmailOnEditProfile()) {
 			$result["errors"] = array("email already exists");
-		}elseif($user->clear()->update() == 1){
+		} elseif ($user->clear()->update() == 1) {
 			$result["success"] = true;
 		}
-	}else{ 
+	} else {
 		$result["errors"] = $user->errors;
 	}
 }
