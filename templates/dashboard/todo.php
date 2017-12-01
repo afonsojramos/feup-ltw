d66c8abdaea3a95a1e7cd8e4dfd083dde072333a
msramalho <up201403027@fe.up.pt>
2017-12-01 22:17:09 +0000

    [todo html] removed unnecessary tag attributes

--- templates/dashboard/todo.php
+++ templates/dashboard/todo.php
@@ -4,11 +4,11 @@
 		<div class="todoListItem">
 			<input class="checkbox todoItem" id = "todoItem_<?= $item->itemId ?>" data-itemId="<?= $item->itemId ?>" type="checkbox"  <?= $item->completed ? "checked" : "" ?>><label class="todoItemLabel" data-itemId="<?= $item->itemId ?>" ><?= $item->content ?></label>
 			<input type="text" class="addItemTextBox hidden" id="editItem_<?= $item->itemId ?>">
-			<i class="material-icons close removeListItem show-on-hover" id="deleteItem_<?= $item->itemId ?>" data-itemId="<?= $item->itemId ?>" type="button">close</i>
+			<i class="material-icons close removeListItem show-on-hover" type="button">close</i>
 		</div>
 	<?php endforeach ?>
 	<div class="addItemContainer">
-		<span> 
+		<span>
 			<i class="material-icons">add</i>
 			<span class="addItemText"> Add a new item</span>
 		</span>
@@ -16,8 +16,8 @@
 	<div class="errors"></div>
 	<hr/>
 	<span class="listFooter show-on-hover">
-		<span class="archive" data-todoListId="<?= $todo->todoListId ?>"><a href="#"><i class="material-icons"><?= $todo->archived ? "unarchive" : "archive" ?></i></a></span>
-		<span class="delete" data-todoListId="<?= $todo->todoListId ?>"><a href="#"><i class="material-icons">delete</i></a></span>
+		<span class="archive"><a href="#"><i class="material-icons"><?= $todo->archived ? "unarchive" : "archive" ?></i></a></span>
+		<span class="delete"><a href="#"><i class="material-icons">delete</i></a></span>
 		<span class="share"><a href="#"><i class="material-icons">share</i></a></span>
 	</span>
 </div>
