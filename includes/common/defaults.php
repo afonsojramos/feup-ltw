b3d76ad70f0d3512a928966ae76af6dc767e3c60
msramalho <up201403027@fe.up.pt>
2017-12-02 00:53:06 +0000

    [defaults] refactored

--- includes/common/defaults.php
+++ includes/common/defaults.php
@@ -1,9 +1,13 @@
 <?php
 	//Set the default values for the properties used
-	$PAGE["cssFolder"] = "public/css/";
-	$PAGE["jsFolder"] = "public/js/";
 	$PAGE["lang"] = "en";
 	$PAGE["title"] = "4Me2Do";
+
+	$PAGE["cssFolder"] = "public/css/";
+	$PAGE["jsFolder"] = "public/js/";
 	$PAGE["styles"] = array("style.css", "navbar.css", "material-icons.css");
 	$PAGE["scripts"] = array("scripts.js");
+
+	$PAGE["includeCSRF"] = false;
+	$PAGE["showSideBar"] = false;
 ?>
