a00bc5cdff59575bdf601753a8c44f6cb1b76674
Daniel Silva <danny@dannyps.net>
2017-12-05 19:43:00 +0000

    Build search object and AJAX it!

--- public/js/search.js
+++ public/js/search.js
@@ -14,6 +14,32 @@ function enterSearch(e){
 
 function doSearch(){
 	if(search == document.activeElement){
-		//TODO: perform search on search.value
+		const regex = /(?:@(\w+))|(?:!(\w+))|(?:#(\w+))|([a-zA-ZÀ-ÖØ-öø-ÿ]+)|(?:\"(.*)\")/g;
+		let m;
+		let res={};
+		while ((m = regex.exec(search.value)) !== null) {
+			// This is necessary to avoid infinite loops with zero-width matches
+			if (m.index === regex.lastIndex) {
+				regex.lastIndex++;
+			}
+			
+			// The result can be accessed through the `m`-variable.
+			m.forEach((match, groupIndex) => {
+				if(match!=undefined && groupIndex!=0){
+					set = 0;
+					console.log(`Found match, group ${groupIndex}: ${match}`);
+					if(!res.hasOwnProperty(groupIndex)){
+						res[groupIndex] = [match];
+					}else{
+						res[groupIndex].push(match)
+					}
+				}
+					
+			});
+		}
+		request("actions/user/search.php", function (result) {
+			/* treat result here */
+		}, res, "post");
+
 	}
 }
