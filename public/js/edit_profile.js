d8a4431bc5eda1b9711dc1c6950e522b378a49ef
msramalho <up201403027@fe.up.pt>
2017-12-06 02:06:13 +0000

    edit_profile javascript refactor

--- public/js/edit_profile.js
+++ public/js/edit_profile.js
@@ -1,3 +1,21 @@
+//edit password ajax
+let form = document.getElementById("changePassword");
+
+form.addEventListener("submit", function (e) {
+	let formData = new FormData(form);
+	data = formDataToAjax(formData);
+	request("actions/user/edit_profile.php", function (result) {
+		if(result.success){
+			addSuccessMessage(form);
+			form.reset();
+		}else{
+			addErrorMessage(form, result.errors);
+		}
+	}, data, "post");
+	e.preventDefault();
+}, false);
+
+//edit profile information
 let form2 = document.getElementById("editProfile");
 
 form2.addEventListener("submit", function (e) {
