80d970cd103f7ae9364208e5f5723118c3e8c1d3
Afonso Jorge Ramos <up201506239@fe.up.pt>
2017-12-07 09:52:44 +0000

    Visual and Mobile

--- templates/common/navbar_guest.php
+++ templates/common/navbar_guest.php
@@ -1,9 +1,9 @@
 <nav class="navbar">
 	<div class="container">
-		<h1 class="logo"><a href="index.php"><i class="material-icons md-36">list</i> 4Me2Do</a></h1>
+		<h1 class="logo"><a href="index.php"><i class="material-icons md-36">dashboard</i> 4Me2Do</a></h1>
 		<ul class="nav navRight">
-			<li><a href="login.php"><i class="material-icons">person_outline</i> Login</a></li>
-			<li><a href="register.php"><i class="material-icons">person_add</i> Register</a></li>
+			<li><a href="login.php"><i class="material-icons">person_outline</i><span class="nav navRight text"> Login</a></span></li>
+			<li><a href="register.php"><i class="material-icons">person_add</i><span class="nav navRight text"> Register</a></span></li>
 		</ul>
 	</div>
 </nav>
