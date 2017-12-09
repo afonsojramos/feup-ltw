cf6920f463642473f776f44f3788d3f7b834a557
msramalho <up201403027@fe.up.pt>
2017-12-09 20:25:22 +0000

    possibly working

--- classes/TodoList.php
+++ classes/TodoList.php
@@ -1,7 +1,6 @@
 <?php
-
-require_once(dirname(__FILE__)."/QueryBuilder.php");
-require_once(dirname(__FILE__)."/Item.php");
+require_once(dirname(__FILE__) . "/QueryBuilder.php");
+require_once(dirname(__FILE__) . "/Item.php");
 class TodoList extends QueryBuilder{
 	public $todoListId;//attributes that do not require update can be public
 	protected $title;
@@ -16,8 +15,8 @@ class TodoList extends QueryBuilder{
 
 	public static $ignoreProperties = array("items");//properties to ignore on the sql building
 	public static $validationRules = array(//validation rules for update method
-		"title"=>"length:3:200",
-		"colour"=>"in:white:red:orange:yellow:green:teal:blue:purple:pink:brown"
+		"title" => "length:3:200",
+		"colour" => "in:white:red:orange:yellow:green:teal:blue:purple:pink:brown"
 	);
 
 	public function __construct($todoListId = null, $title = "", $tags = "", $colour = "white", $archived = 0, $link = "", $userId = -1, $projectId = 0){
@@ -34,7 +33,7 @@ class TodoList extends QueryBuilder{
 	}
 
 	public function __set($name, $value){
-		if($name == "tags"){//REGEX to remove all whitespaces from the tags
+		if ($name == "tags") {//REGEX to remove all whitespaces from the tags
 			$value = preg_replace('/\s+/', '', $value);
 		}
 		return parent::__set($name, $value);
@@ -58,156 +57,176 @@ class TodoList extends QueryBuilder{
 
 	// Determine if query (copy os $_GET) is a search query, or a regular query.
 	private static function isSearchQuery($query){
-		foreach( array('members', 'flags', 'projects', 'words', 'expressions') as $p){
-			if(array_key_exists($p, $query)){
+		foreach (array('members', 'tags', 'projects', 'words', 'expressions') as $p) {
+			if (array_key_exists($p, $query)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	private static function buildSearchQuery($query, $userId, $qb){
+	private static function buildSearchQuery($query, $userId, $qb) {
+		$params = array();
+		$params["userId"] = $userId;
+		$pq = array();
 
-		$params=array();
-		$params['userId']=$userId;
-		$pq=array();
-
-		if(isset($query['words'])){
-			$words=explode(',', $_GET['words']);
-		}else{
-			$words=array();
+		//merge words and expressions into $words
+		if (isset($query["words"])) {
+			$words = explode(',', $query["words"]);
+		} else {
+			$words = array();
 		}
-		if(isset($query['expressions'])){
-			$expressions=explode(',', $_GET['expressions']);
-		}else{
-			$expressions=array();
+		if (isset($query["expressions"])) {
+			$expressions = explode(',', $query["expressions"]);
+		} else {
+			$expressions = array();
 		}
 		$words = array_merge($words, $expressions);
-		
 
-		/**
-		 * (SELECT projectId FROM members WHERE userId IN (
-         *		SELECT userId FROM users WHERE username IN (:username1, username2) AND projectId in (SELECT projectId FROM members WHERE userId = :userId)) --
-         * )
-		 */
-		if(isset($query['members'])){
-			$members=explode(',', $_GET['members']);
-			$pq['members']="SELECT projectId FROM members WHERE userId IN (SELECT userId FROM users WHERE username in (";
+
+		$masterQuery = array();
+		/* SELECT DISTINCT todoListId FROM todolists WHERE projectId IN (
+			SELECT projectId FROM members WHERE userId IN (SELECT userId FROM users WHERE username IN ("msramalho", "dannyps"))
+		) */
+		if (isset($query["members"])) {
+			$members = explode(',', $query["members"]);
+			$subquery = "SELECT DISTINCT todoListId FROM todolists WHERE projectId IN (SELECT projectId FROM members WHERE userId IN (SELECT userId FROM users WHERE username IN (";
 			$i = 0;
-			foreach($members as $m){
-				$pq['members'].=":username{$i}, ";
+			$temp = array();
+			foreach ($members as $m) {
+				$temp[] = ":username{$i}";
 				$params["username{$i}"] = $m;
-
 				$i++;
 			}
-
-			$pq['members']=substr($pq['members'], 0, -1*strlen(", ")); // remove '"'from end of string
-			$pq['members'].=") AND projectId IN (SELECT projectId FROM members WHERE userId = :userId)";
-		}else{
-			$pq['members']="SELECT projectId FROM members WHERE userId IN (SELECT userId FROM users WHERE username in () AND projectId IN (SELECT projectId FROM members WHERE userId = :userId)";
+			$subquery .= implode(", ", $temp);
+			$subquery .= ")))";
+			$masterQuery[] = $subquery;
 		}
 
-		/**
-		 * (SELECT * FROM projects WHERE projectId IN (SELECT projectId FROM members WHERE userId = :currentUser) AND (projects.title IN ("proj1", "proj2")))
-		 */
-		if(isset($query['projects'])){
-			$projects=explode(',', $_GET['projects']);
-			$pq['projects']="SELECT todoListId FROM projects WHERE projectId IN (SELECT projectId FROM members WHERE userId = :userId) AND (projects.title IN (";
-			$i = 0;
-			foreach($projects as $p){
-				$pq['projects'].=":project{$i}, ";
-				$params["project{$i}"] = $p;
-				
-				$i++;
-			}
-			$pq['projects']=substr($pq['projects'], 0, -1*strlen(", ")); // remove ', 'from end of string
-			$pq['projects'].="))";
-		}else{
-			$pq['projects']="SELECT todoListId FROM projects WHERE projectId IN (SELECT projectId FROM members WHERE userId = :userId) AND (projects.title IN ())";
-			
-		}
 
-		/** 	(SELECT todoListId FROM todolists WHERE (
-        *			(tags LIKE "rcom" OR tags LIKE "rcom,%" OR tags LIKE "%,rcom,%" OR tags LIKE "%,rcom") 
-  		*	 	OR
-        *			(tags LIKE "ltw" OR tags LIKE "ltw,%" OR tags LIKE "%,ltw,%" OR tags LIKE "%,ltw") 
-		*		))
+		/*
+		SELECT DISTINCT todoListId FROM todolists WHERE projectId IN (SELECT projectId FROM projects WHERE title IN ("rcom","ltw")
 		*/
-		if(isset($query['tags'])){
+		if (isset($query["projects"])) {
+			$projects = explode(',', $query["projects"]);
+			$subquery = "SELECT DISTINCT todoListId FROM todolists WHERE projectId IN (SELECT projectId FROM projects WHERE title IN (";
+
+			$i = 0;
+			$temp = array();
+			foreach ($projects as $p) {
+				$temp[] = ":project{$i}";
+				$params["project{$i}"] = $p;
+				$i++;
+			}
+			$subquery .= implode(", ", $temp);
+
+			$subquery .= "))";
+			$masterQuery[] = $subquery;
+		}
+
+
+		/*
+		SELECT DISTINCT todoListId FROM todolists WHERE (
+			(tags LIKE "rcom" OR tags LIKE "rcom,%" OR tags LIKE "%,rcom,%" OR tags LIKE "%,rcom")
+			OR
+			(tags LIKE "ltw" OR tags LIKE "ltw,%" OR tags LIKE "%,ltw,%" OR tags LIKE "%,ltw")
+		)
+		*/
+		if (isset($query["tags"])) {
 			//tag(s) are set.
 			//pq stands for partial query
-			$pq['tags']="SELECT todoListId FROM todolists WHERE (";
-			$tags=explode(',', $_GET['tags']);
-			$i=0;
-			foreach($tags as $t){
-				$pq['tags'].="(tags LIKE :tag{$i}0 OR tags LIKE :tag{$i}1 OR tags LIKE :tag{$i}2 OR tags LIKE :tag{$i}3) OR ";
-				$params["tag{$i}0"]="{$t}";
-				$params["tag{$i}1"]="{$t},%";
-				$params["tag{$i}2"]="%,{$t},%";
-				$params["tag{$i}3"]="{$t},%";
+			$subquery = "SELECT DISTINCT todoListId FROM todolists WHERE (";
+			$tags = explode(',', $query["tags"]);
 
+			$i = 0;
+			$temp = array();
+			foreach ($tags as $t) {
+				$temp[] = "tags LIKE :tag{$i}0 OR tags LIKE :tag{$i}1 OR tags LIKE :tag{$i}2 OR tags LIKE :tag{$i}3";
+				$params["tag{$i}0"] = "{$t}";
+				$params["tag{$i}1"] = "%,{$t}";
+				$params["tag{$i}2"] = "%,{$t},%";
+				$params["tag{$i}3"] = "{$t},%";
 				$i++;
 			}
-			$pq['tags']=substr($pq['tags'], 0, -1*strlen(" OR ")); // remove " OR " from end of string
-			$pq['tags'].=")";
-		}else{
-			$pq['tags']="SELECT todoListId FROM todolists WHERE 1"; //verificar esse 1
+			$subquery .= implode(" OR ", $temp);
+			$subquery .= ")";
+			$masterQuery[] = $subquery;
 		}
 
+		/*
+		--title de list
+		SELECT DISTINCT todoListId FROM todolists WHERE (title LIKE "%rcom%" OR title LIKE "%ltw%")
+		--content of items
+		UNION SELECT DISTINCT todoListId FROM items WHERE (content LIKE "%rcom%" OR content LIKE "%ltw%")
+		--title or description of projects
+		UNION SELECT DISTINCT todoListId FROM todolists WHERE projectId IN (SELECT projectId FROM projects WHERE ((title LIKE "%rcom%" OR description LIKE "%rcom%") OR (title LIKE "%ltw%" OR description LIKE "%ltw%"))
+		--tags
+		UNION SELECT DISTINCT todoListId FROM todolists WHERE tags LIKE "%rcom%" OR tags LIKE "%ltw%"
+		*/
+		if (count($words) > 0) {
+			//SELECT DISTINCT todoListId FROM todolists WHERE (title LIKE "%rcom%" OR title LIKE "%ltw%")
+			$subquery = "SELECT DISTINCT todoListId FROM todolists WHERE (";
 
-		/**
-		 * (REPLACE_ME LIKE "%search1%" OR REPLACE_ME LIKE "%search2%"))
-		 * 
-		 */
-
-		 if(count($words)>0){
-			 $pq['words']="(REPLACE_ME LIKE ";
-			 $i = 0;
-			 foreach($words as $w){
-				$pq['words'].=":word{$i} OR REPLACE_ME LIKE ";
-				$params["word{$i}"]=$w;
-
+			$i = 0;
+			$temp = array();
+			foreach ($words as $w) {
+				$temp[] = "title LIKE :word{$i}";
+				$params["word{$i}"] = "%$w%";
 				$i++;
-			 }
-			 $pq["words"]=substr($pq["words"], 0, -1*strlen(" OR REPLACE_ME LIKE ")); // remove " OR REPLACE_ME LIKE " from end of string
-			 $pq['words'].=')';
-		 }else{
-			$pq['words']="1";
-		 }
-		
-		var_dump($pq);
-		var_dump($params);
+			}
+			$subquery .= implode(" OR ", $temp);
 
+			//UNION SELECT DISTINCT todoListId FROM items WHERE (content LIKE "%rcom%" OR content LIKE "%ltw%")
+			$subquery .= ") UNION SELECT DISTINCT todoListId FROM items WHERE (";
 
-		/** |--------------------------------------WHOLE QUERY-----------------------------------|
-		 * 	|																					 |
-		 * 	|-------------------1st sq--------------------|---2nd sq---|---3rd sq---|---4th sq---|
-		 *  |																					 |
-		 *  |----1.1 sq----|----1.2 sq----|----1.3 sq---- |										 |
-		 * 
-		 */
+			$i = 0;
+			$temp = array();
+			foreach ($words as $w) {
+				$temp[] = "content LIKE :word{$i}";
+				$i++;
+			}
+			$subquery .= implode(" OR ", $temp);
 
-		 $pq['sq11']=$pq['members'];
-		 $pq['sq12']=$pq['projects'];
-		 $pq['sq13']="SELECT projectId FROM projects WHERE ".str_replace("REPLACE_ME", "title", $pq['words'])." OR ".str_replace("REPLACE_ME", "description", $pq['words']);
-		 
-		 $pq['sq1']="SELECT todoListId FROM todolists WHERE projectId IN(".$pq['sq11']." UNION ".$pq['sq12']. " UNION " . $pq['sq13']. "))";
-		 
-		 $pq['sq2']=$pq['tags'];
+			//UNION SELECT DISTINCT todoListId FROM todolists WHERE projectId IN (SELECT projectId FROM projects WHERE ((title LIKE "%rcom%" OR description LIKE "%rcom%") OR (title LIKE "%ltw%" OR description LIKE "%ltw%"))
+			$subquery .= ") UNION SELECT DISTINCT todoListId FROM todolists WHERE projectId IN (SELECT projectId FROM projects WHERE (";
 
-		 $pq['sq3']="SELECT todoListId FROM todolists WHERE " . str_replace("REPLACE_ME", "title", $pq['words']);
+			$i = 0;
+			$temp = array();
+			foreach ($words as $w) {
+				$temp[] = "title LIKE :word{$i} OR description LIKE :word{$i}";
+				$i++;
+			}
+			$subquery .= implode(" OR ", $temp);
 
-		 $pq['sq4']="SELECT todoListId FROM items WHERE " . str_replace("REPLACE_ME", "content", $pq['words']);
+			//UNION SELECT DISTINCT todoListId FROM todolists WHERE tags LIKE "%rcom%" OR tags LIKE "%ltw%"
+			$subquery .= ")) UNION SELECT DISTINCT todoListId FROM todolists WHERE (";
 
-		 $fullWhere="todoListId IN (";
-		 $fullWhere.=$pq['sq1']. " UNION ". $pq['sq2']. " UNION ". $pq['sq3']. " UNION ". $pq['sq4']. ")";
+			$i = 0;
+			$temp = array();
+			foreach ($words as $w) {
+				$temp[] = "tags LIKE :word{$i}";
+				$i++;
+			}
+			$subquery .= implode(" OR ", $temp);
 
-		 ini_set('xdebug.var_display_max_depth', 5);
-		 ini_set('xdebug.var_display_max_children', 256);
-		 ini_set('xdebug.var_display_max_data', 1024);
-		 var_dump($pq);
-		 var_dump($fullWhere);
-		return $qb->select()->where($fullWhere)->addParams($params);
+			$subquery .= ")";
+			$masterQuery[] = $subquery;
+		}
+
+		$gluedMasterQuery = implode(" INTERSECT ", $masterQuery);
+		var_dump($masterQuery);
+		// var_dump($params);
+
+		$finalQuery = "SELECT * FROM todolists WHERE todoListId IN (
+			$gluedMasterQuery
+			INTERSECT
+			SELECT DISTINCT todoListId FROM todolists WHERE userId = :userId OR (projectId IN (SELECT m.projectId FROM members as m where userId = :userId))
+		)";
+		echo $finalQuery;
+		if($result = $qb->execute($finalQuery, $params)){
+			return $result->fetchAll(PDO::FETCH_ASSOC);
+		}
+		return false;
 	}
 
 	//all the lists this user can see, query is an array of key values with the possible search values
@@ -216,14 +235,12 @@ class TodoList extends QueryBuilder{
 
 		$qb = new QueryBuilder(self::class);
 
-		if(self::isSearchQuery($query)){
+		if (self::isSearchQuery($query)) {
 			//special build here
-			$searchQuery=self::buildSearchQuery($query, $userId, $qb);
-			$lines = $searchQuery->getAll();
-		}else{
+			$lines = self::buildSearchQuery($query, $userId, $qb);
+		} else {
 			$lines = $qb->select()->where("userId = :userId OR (projectId IN (SELECT m.projectId FROM members as m where userId = :userId))")->addParam("userId", $userId)->getAll();
 		}
-		//TODO: maybe load project information and user
 
 		return self::loadTodoFromDatabase($lines, $loadItemsAsWell);
 	}
@@ -231,7 +248,6 @@ class TodoList extends QueryBuilder{
  	//all the lists this user can see
 	public static function getAllByUser($userId, $loadItemsAsWell = true){
 		$qb = new QueryBuilder(self::class);
-	//TODO: maybe load project information
 		$lines = $qb->select()->where("userId = :userId OR (projectId IN (SELECT m.projectId FROM members as m where userId = :userId))")->addParam("userId", $userId)->getAll();
 		return self::loadTodoFromDatabase($lines, $loadItemsAsWell);
 	}
@@ -242,7 +258,7 @@ class TodoList extends QueryBuilder{
 		$lines = $qb->select()->where("userId = :userId AND projectId = 0")->addParam("userId", $userId)->getAll();
 		return self::loadTodoFromDatabase($lines, $loadItemsAsWell);
 	}
-*/
+	 */
 	//all the lists that are in a project
 	public static function getAllByProject($projectId, $loadItemsAsWell = true){
 		$qb = new QueryBuilder(self::class);
@@ -253,7 +269,9 @@ class TodoList extends QueryBuilder{
 	private static function loadTodoFromDatabase($lines, $loadItemsAsWell = true){
 		if ($loadItemsAsWell) {
 			//get all the ids
-			$ids = array_unique(array_map(function($line){return $line["todoListId"];}, $lines));
+			$ids = array_unique(array_map(function ($line) {
+				return $line["todoListId"];
+			}, $lines));
 			//get all the items for all the lists
 			$items = Item::getAllForLists($ids);
 		}
@@ -263,8 +281,8 @@ class TodoList extends QueryBuilder{
 			$todo->loadFromArray($line);
 			//load respective items
 			foreach ($items as $item)
-				if($item->todoListId == $todo->todoListId)
-					$todo->items[] = $item;
+				if ($item->todoListId == $todo->todoListId)
+				$todo->items[] = $item;
 			$todos[] = $todo;
 		}
 		return $todos;
@@ -275,7 +293,7 @@ class TodoList extends QueryBuilder{
 	 * Returns true if the supplied user has access to this list, wither it's his or it belongs to a project of which he is a member
 	 */
 	public function verifyOwnership($userId){
-		if($this->userId == $userId){//the user is the owner
+		if ($this->userId == $userId) {//the user is the owner
 			return true;
 		}
 		//check if the user has permission for the list
